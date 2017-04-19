3a842932619319750c836ebbd0f484710fa0b5bb
steve white <steve white>
2017-04-19 18:31:53 -0400

    importing from backup

---
+++ script.js
@@ -0,0 +1,421 @@
+// JavaScript Document
+
+
+function loadPage(f) {
+	location.href = nextPage[random_num];
+}
+
+function testResultsOne (form) {
+    TestAnswer = oneString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+		window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsTwo (form) {
+    TestAnswer = twoString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsThree (form) {
+    TestAnswer = threeString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsFour (form) {
+    TestAnswer = fourString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsFive (form) {
+    TestAnswer = fiveString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsSix (form) {
+    TestAnswer = sixString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsSeven (form) {
+    TestAnswer = sevenString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsEight (form) {
+    TestAnswer = oneString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsNine (form) {
+    TestAnswer = twoString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsTen (form) {
+    TestAnswer = threeString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsEleven (form) {
+    TestAnswer = fourString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+
+
+
+
+
+
+// BASS STAFF BELOW
+
+function testResultsBassOne (form) {
+    TestAnswer = oneString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassTwo (form) {
+    TestAnswer = sevenString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassThree (form) {
+    TestAnswer = sixString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassFour (form) {
+    TestAnswer = fiveString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassFive (form) {
+    TestAnswer = fourString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassSix (form) {
+    TestAnswer = threeString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassSeven (form) {
+    TestAnswer = twoString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassEight (form) {
+    TestAnswer = oneString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassNine (form) {
+    TestAnswer = sevenString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassTen (form) {
+    TestAnswer = sixString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+function testResultsBassEleven (form) {
+    TestAnswer = fiveString (form.noteAnswer.value)
+    if (TestAnswer == 1){
+		changeProp('incorrect','','display','none','DIV');
+        changeProp('correct','','display','block','DIV');
+window.setTimeout(loadPage, 1000);	
+	}
+    else {
+		changeProp('correct','','display','none','DIV');
+        changeProp('incorrect','','display','block','DIV');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+function oneString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="cC";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+function twoString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="dD";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+function threeString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="eE";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+function fourString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="fF";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+function fiveString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="gG";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+function sixString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="aA";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+function sevenString (InString)  {
+    if(InString.length==0) return (false);
+    var RefString="bB";
+    for (Count=0; Count < InString.length; Count++)  {
+        TempChar= InString.substring (Count, Count+1);
+        if (RefString.indexOf (TempChar, 0)==-1)  
+            return (false);
+    }
+    return (true);
+}
+
+
+
+
+
+
+
+
+
+function changeProp(objId,x,theProp,theValue) { //v9.0
+  var obj = null; with (document){ if (getElementById)
+  obj = getElementById(objId); }
+  if (obj){
+    if (theValue == true || theValue == false)
+      eval("obj.style."+theProp+"="+theValue);
+    else eval("obj.style."+theProp+"='"+theValue+"'");
+  }
+}
+
+
+nextPage = new Array
+nextPage[1]="piano-notes-1.html"
+nextPage[2]="piano-notes-2.html"
+nextPage[3]="piano-notes-3.html"
+nextPage[4]="piano-notes-4.html"
+nextPage[5]="piano-notes-5.html"
+nextPage[6]="piano-notes-6.html"
+nextPage[7]="piano-notes-7.html"
+nextPage[8]="piano-notes-8.html"
+nextPage[9]="piano-notes-9.html"
+nextPage[10]="piano-notes-10.html"
+nextPage[11]="piano-notes-11.html"
+nextPage[12]="piano-notes-b1.html"
+nextPage[13]="piano-notes-b2.html"
+nextPage[14]="piano-notes-b3.html"
+nextPage[15]="piano-notes-b4.html"
+nextPage[16]="piano-notes-b5.html"
+nextPage[17]="piano-notes-b6.html"
+nextPage[18]="piano-notes-b7.html"
+nextPage[19]="piano-notes-b8.html"
+nextPage[20]="piano-notes-b9.html"
+nextPage[21]="piano-notes-b10.html"
+nextPage[22]="piano-notes-b11.html"
+
+randLocation = new Array
+randLocation[1]="10px"
+randLocation[2]="40px"
+randLocation[3]="70px"
+randLocation[4]="30px"
+randLocation[5]="150px"
+
+
+random_num = (Math.round((Math.random()*21)+1));
+
+
+
